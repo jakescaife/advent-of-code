cd627972f92dfac2e817750975781ec04f061d31
Jake Scaife <jakescaife@pm.me>
2021-05-15 23:59:41 +0100

    Complete 2016-25 part one

---
+++ 2016/2016-12-25/src/bin/part_one.rs
@@ -0,0 +1,150 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    println!("AOC 2016-25 Part One: {}", solve_puzzle(&input));
+}
+
+fn solve_puzzle(input: &str) -> i32 {
+    for initial in 0.. {
+        let mut computer = Computer::default();
+        *computer.memory.register('a') = initial;
+
+        if computer.execute_instructions(input) {
+            return initial;
+        }
+    }
+
+    unreachable!()
+}
+
+#[derive(Default)]
+struct Computer {
+    counter: usize,
+    memory: Memory,
+}
+
+impl Computer {
+    fn execute_instructions(&mut self, instructions: &str) -> bool {
+        let instructions: Vec<Instruction> = instructions.lines()
+            .map(Instruction::from)
+            .collect();
+
+        let mut outputs = Vec::new();
+
+        while let Some(instruction) = instructions.get(self.counter) {
+            match instruction {
+                Instruction::Copy(x, y) => self.memory.copy(*x, *y),
+                Instruction::Increment(x) => self.memory.increment(*x,  1),
+                Instruction::Decrement(x) => self.memory.increment(*x, -1),
+                Instruction::JumpNotZero(x, y) => self.counter_jump(*x, *y),
+                Instruction::Out(x) => {
+                    let output = match x {
+                        Parameter::Register(x) => self.memory.value(*x).unwrap(),
+                        Parameter::Value(x) => *x,
+                    };
+
+                    if output != 0 && output != 1 { return false; }
+
+                    outputs.push(output);
+
+                    if outputs.len() == 10 { break; }
+                }
+            }
+
+            if !matches!(instruction, Instruction::JumpNotZero(_,_)) {
+                self.counter += 1;
+            }
+        }
+
+        outputs.chunks(2).all(|x| x[0] == 0 && x[1] == 1)
+    }
+
+    fn counter_jump(&mut self, parameter: Parameter, offset: Parameter) {
+        let value = match parameter {
+            Parameter::Register(x) => *self.memory.register(x),
+            Parameter::Value(x) => x,
+        };
+
+        let offset = match offset {
+            Parameter::Value(x) => x,
+            Parameter::Register(_) => panic!("Attempt to use register as jump offset"),
+        };
+
+        let offset = match value { 0 => 1, _ => offset };
+
+        self.counter = (self.counter as i32 + offset) as usize;
+    }
+}
+
+#[derive(Default)]
+struct Memory {
+    memory: HashMap<char, i32>,
+}
+
+impl Memory {
+    fn copy(&mut self, first: Parameter, second: Parameter) {
+        let value = match first {
+            Parameter::Register(x) => self.value(x).unwrap(),
+            Parameter::Value(x) => x,
+        };
+
+        match second {
+            Parameter::Register(x) => *self.register(x) = value,
+            Parameter::Value(_) => panic!("Cannot copy to value"),
+        }
+    }
+
+    fn increment(&mut self, parameter: Parameter, amount: i32) {
+        match parameter {
+            Parameter::Register(x) => *self.register(x) += amount,
+            Parameter::Value(_) => panic!("Attempt to modify non-register"),
+        }
+    }
+
+    fn value(&self, register: char) -> Option<i32> {
+        self.memory.get(&register).copied()
+    }
+
+    fn register(&mut self, register: char) -> &mut i32 {
+        self.memory.entry(register).or_default()
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Instruction {
+    Copy(Parameter, Parameter),
+    Increment(Parameter),
+    Decrement(Parameter),
+    JumpNotZero(Parameter, Parameter),
+    Out(Parameter)
+}
+
+impl From<&str> for Instruction {
+    fn from(instruction: &str) -> Self {
+        let slice: Vec<&str> = instruction.split_whitespace().collect();
+        match slice[0] {
+            "cpy" => Self::Copy(slice[1].into(), slice[2].into()),
+            "inc" => Self::Increment(slice[1].into()),
+            "dec" => Self::Decrement(slice[1].into()),
+            "jnz" => Self::JumpNotZero(slice[1].into(), slice[2].into()),
+            "out" => Self::Out(slice[1].into()),
+            _ => panic!("Unexpected instruction"),
+        }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Parameter {
+    Register(char),
+    Value(i32),
+}
+
+impl From<&str> for Parameter {
+    fn from(parameter: &str) -> Self {
+        match parameter.parse::<i32>() {
+            Ok(x) => Self::Value(x),
+            Err(_) => Self::Register(parameter.chars().next().unwrap())
+        }
+    }
+}
