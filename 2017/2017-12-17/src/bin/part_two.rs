f04a6cb18b42c8dded602aa190afaab5bb9d96de
Jake Scaife <jakescaife@pm.me>
2021-08-03 09:37:56 +0100

    Simplify 2017-17 part two

--- 2017/2017-12-17/src/bin/part_two.rs
+++ 2017/2017-12-17/src/bin/part_two.rs
@@ -1,17 +1,13 @@
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    println!("AOC 2017-17 Part Two: {}", solve_puzzle(&input));
-}
-
-fn solve_puzzle(input: &str) -> usize {
     let steps: usize = input.trim().parse().unwrap();
     let mut index = 0;
-    let mut output = 0;
+    let mut solution = 0;
 
     for length in 1..50000000 {
-        index = (index + steps) % length + 1;
-        if index == 1 { output = length }
+        index = 1 + (index + steps) % length;
+        if index == 1 { solution = length }
     }
 
-    output
+    println!("AOC 2017-17 Part Two: {}", solution);
 }
